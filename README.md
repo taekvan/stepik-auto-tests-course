e2c7c7fa35a4f5e9499636022ce9ed1fb4ec04ce
taekvan <taekvandovan@gmail.com>
2020-11-29 18:40:58 +0300

    adding opening file autotest

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # stepik-auto-tests-course
 Stepik auto test repositoty
 New information about stepik courses will be added soon
+1) first changing - adding personal data
+2) second adding information belong to prosecutor
---
+++ file_open.py
@@ -0,0 +1,7 @@
+import os 
+
+current_dir = os.path.abspath(os.path.dirname(__file__))    # получаем путь к директории текущего исполняемого файла 
+file_path = os.path.join(current_dir, 'file.txt')           # добавляем к этому пути имя файла 
+#element.send_keys(file_path)
+print(current_dir)
+print(file_path)
